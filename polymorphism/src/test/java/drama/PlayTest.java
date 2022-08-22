c8753d873a302196470b54634a5fd4d0f70cb464
Yongran <maiyongran@gmail.com>
2022-08-22 16:55:42 +0100

    setup for PlayTest

--- polymorphism/src/test/java/drama/PlayTest.java
+++ polymorphism/src/test/java/drama/PlayTest.java
@@ -1,4 +1,13 @@
 package drama;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class PlayTest {
+
+    private Play play;
+
+    @BeforeEach
+    public void setUp(){
+        play = new Play("Harry Potter and the Cursed Child", "John Tiffany", "The Palace Theatre");
+    }
 }
