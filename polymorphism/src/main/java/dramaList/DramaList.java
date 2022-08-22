ccb899c6f5570e2f93844abd1492dd75942a8189
Yongran <maiyongran@gmail.com>
2022-08-22 17:42:00 +0100

    add properties of DramaList

--- polymorphism/src/main/java/dramaList/DramaList.java
+++ polymorphism/src/main/java/dramaList/DramaList.java
@@ -1,4 +1,14 @@
 package dramaList;
 
+import collection.Collection;
+
 public class DramaList {
+
+    private Collection watchList;
+    private Collection watchedList;
+
+    public DramaList(Collection watchList, Collection watchedList){
+        this.watchList = watchList;
+        this.watchedList = watchedList;
+    }
 }
