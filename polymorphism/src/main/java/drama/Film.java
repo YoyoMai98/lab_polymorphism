ff88a728a6470e70541d3fbdb04debc67d1ec3aa
Yongran <maiyongran@gmail.com>
2022-08-22 15:39:01 +0100

    override nowShowing method in Film

--- polymorphism/src/main/java/drama/Film.java
+++ polymorphism/src/main/java/drama/Film.java
@@ -8,4 +8,13 @@ public class Film extends Drama {
         super(title, director);
         this.isReplay = isReplay;
     }
+
+    @Override
+    public String nowShowing(){
+        if(isReplay == true){
+            return "The movie is back on cinema!";
+        }else{
+            return "Watch it at home.";
+        }
+    }
 }
