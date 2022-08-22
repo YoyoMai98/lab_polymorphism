05db52b8b8c6e2f097baf817e908b2881b8f181b
Yongran <maiyongran@gmail.com>
2022-08-22 15:23:46 +0100

    setup for DramaTest

--- polymorphism/src/test/java/drama/DramaTest.java
+++ polymorphism/src/test/java/drama/DramaTest.java
@@ -1,4 +1,13 @@
 package drama;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class DramaTest {
+
+    private Drama drama;
+
+    @BeforeEach
+    public void setUp(){
+        drama = new Drama("Bullet Train", "David Leitch");
+    }
 }
