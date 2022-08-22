1888f618a6041cc0a45db1c6a2f522fa2d05ae88
Yongran <maiyongran@gmail.com>
2022-08-22 14:45:53 +0100

    add properties of Drama

--- polymorphism/src/main/java/drama/Drama.java
+++ polymorphism/src/main/java/drama/Drama.java
@@ -1,4 +1,14 @@
 package drama;
 
 public class Drama {
+
+    private String title;
+    private String director;
+    private boolean isShown;
+
+    public Drama(String title, String director){
+        this.title = title;
+        this.director = director;
+        this.isShown = true;
+    }
 }
