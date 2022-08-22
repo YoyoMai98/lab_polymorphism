ea3222ee1dc62140cb28f9f944459bfe475bd46b
Yongran <maiyongran@gmail.com>
2022-08-22 16:24:14 +0100

    setup for TelevisionTest

--- polymorphism/src/test/java/drama/TelevisionTest.java
+++ polymorphism/src/test/java/drama/TelevisionTest.java
@@ -1,4 +1,10 @@
 package drama;
 
 public class TelevisionTest {
+
+    private Television tv;
+
+    public void setUp(){
+        tv = new Television("Person of Interest", "Jonathan Nolan","America");
+    }
 }
