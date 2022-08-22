25113049a76e549a04b918b9efef17000fc953a4
Yongran <maiyongran@gmail.com>
2022-08-22 16:26:11 +0100

    test for showName method

--- polymorphism/src/test/java/drama/TelevisionTest.java
+++ polymorphism/src/test/java/drama/TelevisionTest.java
@@ -1,10 +1,23 @@
 package drama;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TelevisionTest {
 
     private Television tv;
 
+    @BeforeEach
     public void setUp(){
         tv = new Television("Person of Interest", "Jonathan Nolan","America");
+    }
+
+    @Test
+    public void canShowName(){
+        String expected = "The drama is called Person of Interest.";
+        String actual = tv.showName();
+        assertEquals(actual, expected);
     }
 }
