47f73b2118b1a4272a52531f5760aeb18ef7daeb
Yongran <maiyongran@gmail.com>
2022-08-22 16:57:16 +0100

    tests for showName and nowShowing methods

--- polymorphism/src/test/java/drama/PlayTest.java
+++ polymorphism/src/test/java/drama/PlayTest.java
@@ -1,6 +1,9 @@
 package drama;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayTest {
 
@@ -9,5 +12,27 @@ public class PlayTest {
     @BeforeEach
     public void setUp(){
         play = new Play("Harry Potter and the Cursed Child", "John Tiffany", "The Palace Theatre");
+    }
+
+    @Test
+    public void canShowName(){
+        String expected = "The drama is called Harry Potter and the Cursed Child.";
+        String actual = play.showName();
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void canNowShowing_true(){
+        String actual = play.nowShowing();
+        String expected = "It is showing now!";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void canNowShowing_false(){
+        play.setIsShown(false);
+        String actual = play.nowShowing();
+        String expected = "Oh no, it closed.";
+        assertEquals(actual, expected);
     }
 }
