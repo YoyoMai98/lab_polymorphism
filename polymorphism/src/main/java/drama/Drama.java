b59617f4a46cf3a94c872aae54857f9c3485b506
Yongran <maiyongran@gmail.com>
2022-08-22 15:10:02 +0100

    add getters & setters

--- polymorphism/src/main/java/drama/Drama.java
+++ polymorphism/src/main/java/drama/Drama.java
@@ -11,4 +11,29 @@ public class Drama {
         this.director = director;
         this.isShown = true;
     }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDirector(){
+        return director;
+    }
+
+    public void setDirector(String director){
+        this.director = director;
+    }
+
+    public boolean getIsShown(){
+        return isShown;
+    }
+
+    public void setIsShown(boolean isShown){
+        this.isShown = isShown;
+    }
+
 }
