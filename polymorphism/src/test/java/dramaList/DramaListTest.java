e3c96135287f546284373d8afc618598f7603726
Yongran <maiyongran@gmail.com>
2022-08-22 18:31:52 +0100

    add test for addDramaToCollection

--- polymorphism/src/test/java/dramaList/DramaListTest.java
+++ polymorphism/src/test/java/dramaList/DramaListTest.java
@@ -1,7 +1,11 @@
 package dramaList;
 
 import collection.Collection;
+import drama.Television;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DramaListTest {
 
@@ -14,5 +18,14 @@ public class DramaListTest {
         watchedList = new Collection("tv");
         watchList = new Collection("tv");
         fullList = new DramaList(watchList, watchList);
+    }
+
+    @Test
+    public void canAddDramaToCollection(){
+        Television poi = new Television("Person of Interest", "Jonathan Nolan","America");
+        fullList.addDramaToCollection(poi, watchedList);
+        int expected = 1;
+        int actual = watchedList.countDramaCollection();
+        assertEquals(actual,expected);
     }
 }
