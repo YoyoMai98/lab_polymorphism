0e766754e3844978d51b61b51fb997c677576a66
Yongran <maiyongran@gmail.com>
2022-08-22 17:02:21 +0100

    add properties in Collection

--- polymorphism/src/main/java/collection/Collection.java
+++ polymorphism/src/main/java/collection/Collection.java
@@ -1,4 +1,16 @@
 package collection;
 
+import drama.Drama;
+
+import java.util.ArrayList;
+
 public class Collection {
+
+    private String type;
+    private ArrayList<Drama> dramaCollection;
+
+    public Collection(String type){
+        this.type = type;
+        dramaCollection = new ArrayList<>();
+    }
 }
