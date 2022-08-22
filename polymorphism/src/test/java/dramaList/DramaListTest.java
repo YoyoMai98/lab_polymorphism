df9b57a5c366df51bd6b29ad7161fdb3cf820496
Yongran <maiyongran@gmail.com>
2022-08-22 18:23:14 +0100

    setup for DramaListTest

--- polymorphism/src/test/java/dramaList/DramaListTest.java
+++ polymorphism/src/test/java/dramaList/DramaListTest.java
@@ -1,4 +1,18 @@
 package dramaList;
 
+import collection.Collection;
+import org.junit.jupiter.api.BeforeEach;
+
 public class DramaListTest {
+
+    private DramaList fullList;
+    private Collection watchList;
+    private Collection watchedList;
+
+    @BeforeEach
+    public void setUp(){
+        watchedList = new Collection("tv");
+        watchList = new Collection("tv");
+        fullList = new DramaList(watchList, watchList);
+    }
 }
