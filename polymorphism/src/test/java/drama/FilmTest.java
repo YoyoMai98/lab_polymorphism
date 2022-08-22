1b5531a68100a3e526eaaf04344061cd33b9f6ba
Yongran <maiyongran@gmail.com>
2022-08-22 15:42:01 +0100

    setup for FilmTest

--- polymorphism/src/test/java/drama/FilmTest.java
+++ polymorphism/src/test/java/drama/FilmTest.java
@@ -1,4 +1,13 @@
 package drama;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class FilmTest {
+
+    private Film film;
+
+    @BeforeEach
+    public void setUp(){
+        film = new Film("The Lord of the Rings", "Peter Jackson", true);
+    }
 }
