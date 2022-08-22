5a48afae9448e29516fca3faafa7f6aa8ad64534
Yongran <maiyongran@gmail.com>
2022-08-22 17:09:16 +0100

    setup for CollectionTest

--- polymorphism/src/test/java/collection/CollectionTest.java
+++ polymorphism/src/test/java/collection/CollectionTest.java
@@ -1,4 +1,17 @@
 package collection;
 
+import drama.Drama;
+import drama.Television;
+import org.junit.jupiter.api.BeforeEach;
+
 public class CollectionTest {
+
+    private Drama poi;
+    private Drama cm;
+
+    @BeforeEach
+    public void setUp(){
+        poi = new Television("Person of Interest", "Jonathan Nolan","America");
+        cm = new Television("Criminal Minds", "Glenn Kershaw", "America");
+    }
 }
