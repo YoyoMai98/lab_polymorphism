be043cff645f0b2903c01d65a27ed5736ede92b0
Yongran <maiyongran@gmail.com>
2022-08-22 17:13:38 +0100

    add a variable

--- polymorphism/src/test/java/collection/CollectionTest.java
+++ polymorphism/src/test/java/collection/CollectionTest.java
@@ -3,15 +3,18 @@ package collection;
 import drama.Drama;
 import drama.Television;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CollectionTest {
 
     private Drama poi;
     private Drama cm;
+    private Collection collection;
 
     @BeforeEach
     public void setUp(){
         poi = new Television("Person of Interest", "Jonathan Nolan","America");
         cm = new Television("Criminal Minds", "Glenn Kershaw", "America");
+        collection = new Collection("tv");
     }
 }
